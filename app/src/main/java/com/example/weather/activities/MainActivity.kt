005a435d70b45c923eb22633db1f06e797fbcaea
Sudip <sudipbiswas322@gmail.com>
2022-06-10 14:48:28 +0530

    search integrated

--- app/src/main/java/com/example/weather/activities/MainActivity.kt
+++ app/src/main/java/com/example/weather/activities/MainActivity.kt
@@ -6,61 +6,51 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.weather.R
+import com.example.weather.fragments.CurrentWeather
+import com.example.weather.fragments.FavoritesWeather
+import com.example.weather.fragments.SearchWeather
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
-    var weather_url = ""
-    var weather_api = "1395a55568374c5d97143d3bae3473e4"
-    private lateinit var textView: TextView
-    private lateinit var txtWeather : TextView
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.txtWeatherDescription)
-        txtWeather = findViewById(R.id.txtWeather)
-        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this)
-        getWeather()
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
 
-    }
-    @SuppressLint("MissingPermission")
-    fun getWeather(){
-        Log.e("lat", "function")
-        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
-            if(location != null){
-                weather_url = "https://api.weatherbit.io/v2.0/current?" + "lat=" + location?.latitude + "&lon=" + location?.longitude + "&key=" + weather_api
-                Log.e("lat", weather_url.toString())
-                getTemp()
+        val firstFragment = CurrentWeather()
+        val secondFragment = SearchWeather()
+        val thirdFragment = FavoritesWeather()
+
+        setCurrentFragment(firstFragment)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_current_weather -> setCurrentFragment(firstFragment)
+
+                R.id.action_search -> {
+                    setCurrentFragment(secondFragment)
+                }
+                R.id.action_favorites -> {
+                    setCurrentFragment(thirdFragment)
+                }
             }
-            else{
-                Toast.makeText(this, "No location found", Toast.LENGTH_SHORT).show()
-            }
+            true
         }
-    }
-    fun getTemp(){
-        val queue = Volley.newRequestQueue(this)
-        val url: String = weather_url
-        Log.d("url", url)
-        val stringReq = StringRequest(Request.Method.GET, url, { response ->
-            val jsonObject = JSONObject(response)
-            val jsonArray = jsonObject.getJSONArray("data")
-            val jsonObject1 = jsonArray.getJSONObject(0)
-            val temp = jsonObject1.getString("temp")
-            val city = jsonObject1.getString("city_name")
-            Log.d("Temp", temp)
-            txtWeather.text = "Today's weather at "+city+" is:"
-            textView.text = temp + "°C"
-        }, { error ->
-            Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
-        })
-        queue.add(stringReq)
-    }
 
+    }
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame,fragment)
+            commit()
+        }
 }
--- app/src/main/java/com/example/weather/fragments/SearchWeather.kt
+++ app/src/main/java/com/example/weather/fragments/SearchWeather.kt
@@ -8,54 +8,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.weather.R
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import org.json.JSONObject
 
-
-class CurrentWeather : Fragment() {
-
-    var weather_url = ""
+class SearchWeather : Fragment() {
     var weather_api = "1395a55568374c5d97143d3bae3473e4"
+    private lateinit var searchBar: EditText
+    private lateinit var searchButton: Button
     private lateinit var txtCity: TextView
     private lateinit var txtTemp: TextView
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_current_weather, container, false)
-        txtTemp = view.findViewById(R.id.txtTemp)
-        txtCity = view.findViewById(R.id.txtCity)
-        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(activity as Context)
-        getWeather()
+        val view= inflater.inflate(R.layout.fragment_search_weather, container, false)
+        searchBar = view.findViewById(R.id.search_bar)
+        searchButton = view.findViewById(R.id.search_button)
+        txtCity = view.findViewById(R.id.txtWeather)
+        txtTemp = view.findViewById(R.id.txtWeatherDescription)
 
+        searchButton.setOnClickListener {
+            val city = searchBar.text.toString()
+            getWeather(city)
+        }
         return view
     }
     @SuppressLint("MissingPermission")
-    fun getWeather(){
-        Log.e("lat", "function")
-        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
-            if(location != null){
-                weather_url = "https://api.weatherbit.io/v2.0/current?" + "lat=" + location?.latitude + "&lon=" + location?.longitude + "&key=" + weather_api
-                Log.e("lat", weather_url.toString())
-                getTemp()
-            }
-            else{
-                Toast.makeText(activity as Context, "No location found", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-    fun getTemp(){
+    fun getWeather(city: String) {
         val queue = Volley.newRequestQueue(activity as Context)
-        val url: String = weather_url
+        val url: String = "https://api.weatherbit.io/v2.0/current?&city="+city+"&key="+weather_api
         Log.d("url", url)
         val stringReq = StringRequest(Request.Method.GET, url, { response ->
             val jsonObject = JSONObject(response)
@@ -73,7 +62,4 @@ class CurrentWeather : Fragment() {
         })
         queue.add(stringReq)
     }
-
-
 }
-
